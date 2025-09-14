d318c7a928c2d17ab5ce23b8be3f01112b903479
sonukushwaha51 <sonukushwaha51@gmail.com>
2025-09-14 19:07:07 +0530

    Initial commit

---
+++ src/main/java/com/spring/boot/labs/oauth/security/SpringBootOauthSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.labs.oauth.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauthSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOauthSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/boot/labs/oauth/security/SpringBootOauthSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.labs.oauth.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauthSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
