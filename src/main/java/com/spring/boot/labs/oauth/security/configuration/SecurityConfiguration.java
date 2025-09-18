b04c9b72eeae794e89f43df3bb0e5a9e215f7e1c
sonukushwaha51 <sonukushwaha51@gmail.com>
2025-09-18 21:52:26 +0530

    Added UserDetailsService

--- src/main/java/com/spring/boot/labs/oauth/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/spring/boot/labs/oauth/security/configuration/SecurityConfiguration.java
@@ -1,33 +1,55 @@
 package com.spring.boot.labs.oauth.security.configuration;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.http.HttpServletRequest;
-
+@SuppressWarnings("unused")
 @Configuration
 public class SecurityConfiguration {
 
-    // @Override
-    // public List<Filter> getFilters() {
-    //     return new ArrayList<>();
-    // }
-
-    // @Override
-    // public boolean matches(HttpServletRequest request) {
-        
-    //     return false;
-    // }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(request -> 
+            request
+            .requestMatchers("/v1/users/customer-signup","/v1/users/home").permitAll()
+            .anyRequest().authenticated())
+        .httpBasic(Customizer.withDefaults())
+        .formLogin(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    /*
+     * Implementation of providing UserDetailsService Bean using InMemoryUserDetailsManager
+     */
+
+    // @Bean
+    // public UserDetailsService userDetailsService() {
+    //     UserDetails user1 = User.withUsername("test")
+    //                             .password("{noop}test123")
+    //                             .roles("USER")
+    //                             .build();
+
+    //     UserDetails user2 = User.withUsername("test2")
+    //                             .password("{noop}test1234")
+    //                             .roles("USER")
+    //                             .build();
+    //     return new InMemoryUserDetailsManager(user1, user2);
+
+    // }
 
 }
--- src/main/java/com/spring/boot/labs/oauth/security/service/UserService.java
+++ src/main/java/com/spring/boot/labs/oauth/security/service/UserService.java
@@ -3,10 +3,12 @@ package com.spring.boot.labs.oauth.security.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.stereotype.Service;
 
 import com.spring.boot.labs.oauth.security.entity.User;
@@ -17,6 +19,10 @@ public class UserService implements UserDetailsService{
 
     @Autowired
     private UserRepository userRepository;
+
+    AuthenticationManager manager;
+
+    AuthenticationFilter filter;
 
     @Autowired
     BCryptPasswordEncoder passwordEncoder;
