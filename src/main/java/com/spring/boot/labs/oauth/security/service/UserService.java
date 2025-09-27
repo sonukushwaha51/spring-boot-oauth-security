23fa8bb552378615fd7b1f2f47df748cefc830f1
sonukushwaha51 <sonukushwaha51@gmail.com>
2025-09-27 17:37:05 +0530

    Refactored UserService

--- src/main/java/com/spring/boot/labs/oauth/security/service/UserService.java
+++ src/main/java/com/spring/boot/labs/oauth/security/service/UserService.java
@@ -2,7 +2,6 @@ package com.spring.boot.labs.oauth.security.service;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
