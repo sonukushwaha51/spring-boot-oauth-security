5181659447ec0956cab983b7641bc51bb199d2b2
sonukushwaha51 <sonukushwaha51@gmail.com>
2025-10-05 21:19:43 +0530

    Added oauth client properties

--- src/main/java/com/spring/boot/labs/oauth/security/configuration/SecurityConfiguration.java
+++ src/main/java/com/spring/boot/labs/oauth/security/configuration/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package com.spring.boot.labs.oauth.security.configuration;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -15,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 @RequiredArgsConstructor
 @EnableMethodSecurity
+@Slf4j
 public class SecurityConfiguration {
 
     private final JwtAuthFilter jwtAuthFilter;
@@ -34,10 +40,19 @@ public class SecurityConfiguration {
                         .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated())
                         .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                        .oauth2Login(oauth -> oauth.successHandler(this::oauthSuccessHandler).failureHandler((this::oauthFailureHandler)))
                         .build();
         } catch (Exception exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    public void oauthSuccessHandler(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+
+    }
+
+    public void oauthFailureHandler(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) {
+        log.error("Oauth2 Server error: {}", exception.getMessage());
     }
 
 }
