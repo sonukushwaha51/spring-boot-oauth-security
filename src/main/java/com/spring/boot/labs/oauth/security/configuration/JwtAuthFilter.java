b68bfe4f46abe5f719e6c462dec0ece3456217ee
sonukushwaha51 <sonukushwaha51@gmail.com>
2025-12-21 17:54:46 +0530

    change context path

--- src/main/java/com/spring/boot/labs/oauth/security/configuration/JwtAuthFilter.java
+++ src/main/java/com/spring/boot/labs/oauth/security/configuration/JwtAuthFilter.java
@@ -35,6 +35,14 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     }
 
     @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.startsWith("/v1/oauth2/")
+                || path.startsWith("/v1/login")
+                || path.startsWith("/v1/actuator/");
+    }
+
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         
